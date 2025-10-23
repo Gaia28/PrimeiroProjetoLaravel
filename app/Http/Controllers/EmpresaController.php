4b158470821f7126e21d6377e0a4152de8f54164
Gaia28 <matheusgaia33@gmail.com>
2025-10-23 20:21:18 -0300

    novos controllers

---
+++ app/Http/Controllers/EmpresaController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Empresas;
+
+use Illuminate\Http\Request;
+
+class EmpresaController extends Controller
+{
+    public readonly Empresas $empresa;
+    public function __construct()
+    {
+        $this->empresa = new Empresas();
+
+    }
+    public function index()
+    {
+        
+    }
+
+   
+    public function create()
+    {
+        return view('auth/cadastroEmpresa');
+
+    }
+
+    
+    public function store(Request $request)
+    {
+        $request->validate([
+            'razao-social' => 'required|string|max:255',
+            'cnpj' => 'required|string|max:20',
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+        $this->empresa->create([
+            'razao-social' => $request->input('razao-social'),
+            'cnpj' => $request->input('cnpj'),
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password')),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+            return redirect()->back()->with('success', 'Empresa cadastrada com sucesso.');
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
