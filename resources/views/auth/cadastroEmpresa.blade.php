d122cfa5e257e60037c1b86bc2153447861efdad
Gaia28 <matheusgaia33@gmail.com>
2025-10-23 20:26:15 -0300

    views

---
+++ resources/views/auth/cadastroEmpresa.blade.php
@@ -0,0 +1,51 @@
+<script src="https://cdn.tailwindcss.com"></script>
+
+<body class="bg-gray-100 min-h-screen flex">
+
+    <div class="md:flex w-1/2 flex-col items-center justify-center">
+       <h1 class="text-5xl font-extrabold mb-4 text-center text-blue-800">
+            Bem-vindo ao <br> Cadastro Empresarial
+        </h1>
+        <p class="text-lg text-center max-w-md text-blue-500">
+            Crie sua conta para acessar o painel e gerenciar seus dados empresariais com facilidade e segurança.
+        </p>
+    </div>
+
+    <div class="w-full md:w-1/2 flex flex-col items-center justify-center  bg-white shadow-lg">
+        
+        <h2 class="text-2xl font-bold mb-8 text-center text-gray-800">Informe seus dados</h2>
+        
+        <form method="POST" action="cadastro-empresa">
+            @csrf
+            <div class="mb-6">
+                 <label for ="name" class="block text-gray-700 text-sm font-bold mb-2">Razão social</label>
+                <input type="text" id="razao-social" name="razaoSocial" required 
+                       class="w-full px-20 py-2.5 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
+            </div>
+
+            <div class="mb-6">
+                 <label for ="name" class="block text-gray-700 text-sm font-bold mb-2">CNPJ</label>
+                <input type="text" id="cnpj" name="cnpj" required 
+                       class="w-full px-20 py-2.5 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
+            </div>
+            
+            <div class="mb-4">
+                <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email:</label>
+                <input type="email" id="email" name="email" required 
+                       class="w-full  py-2.5 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
+            </div>
+
+            <div class="mb-6">
+                <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Senha:</label>
+                <input type="password" id="password" name="password" required
+                       class="w-full py-2.5 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
+            </div>
+
+            <button type="submit" 
+                    class="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-200">
+                Entrar
+            </button>
+        </form>
+    </div>
+
+</body>
