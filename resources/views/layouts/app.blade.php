85be43f7c71947c2ffcf7e4ec2f7c889523b25c0
Gaia28 <matheusgaia33@gmail.com>
2025-10-24 23:25:52 -0300

    novo arquivo de layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Minha Aplicação Laravel')</title>
+</head>
+<body>
+    <script src="https://cdn.tailwindcss.com"></script>
+    @yield('content')
+</body>
+</html>
