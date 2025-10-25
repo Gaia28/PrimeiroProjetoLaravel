acc6df6c08ca7617e19973dd69fffd0d82782d79
Gaia28 <matheusgaia33@gmail.com>
2025-10-24 23:26:48 -0300

    seedeer de empresa

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Empresas;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,10 +17,21 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        Empresas::factory(5)->create();
 
-        User::factory()->create([
+        Empresas::factory()->create([
+            'razao-social' => 'Empresa Exemplo',
+            'email'       => 'teste@teste.com',
+            'cnpj'        => '000000000/0001-00',
+            'password'    => '123456',
+            'created_at'  => now(),
+            'updated_at'  => now(),
+            
+        ]);
+
+       /* User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+        ]);*/
     }
 }
